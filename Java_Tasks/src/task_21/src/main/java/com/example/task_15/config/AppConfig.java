c526464f7cbf1c3a7474939ee3a864300b77dc5d
Yakobchuk Kirill <69957411+SmartTofu@users.noreply.github.com>
2024-05-01 21:33:41 +0300

    add task_21

---
+++ Java_Tasks/src/task_21/src/main/java/com/example/task_15/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.example.task_15.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableAsync
+@EnableTransactionManagement
+public class AppConfig {
+}
