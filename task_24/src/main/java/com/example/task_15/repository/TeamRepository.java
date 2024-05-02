28485f2ec2deb8313ba85d669550c2a9f23aee12
Yakobchuk Kirill <69957411+SmartTofu@users.noreply.github.com>
2024-05-02 16:57:13 +0300

    Delete task_24 directory

--- task_24/src/main/java/com/example/task_15/repository/TeamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.task_15.repository;
-
-import com.example.task_15.entity.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeamRepository extends JpaRepository<Team, Long> {
-}
